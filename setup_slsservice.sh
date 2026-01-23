4aab3a711395c5b0c617554dacfaa7b2c5f624d0
Eph777 <ephraim7bch@gmail.com>
2026-01-23 16:44:49 +0100

    setup luanti server as a service

--- setup_slsservice.sh
+++ setup_slsservice.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+WORLD="$1"
+
+sudo tee /etc/systemd/system/luanti-server.service > /dev/null <<EOF
+[Unit]
+Description=Luanti Server (Port 30000 default)
+
+[Service]
+Type=simple
+User=${SERVICE_USER}
+WorkingDirectory=/snap/bin
+ExecStart=/snap/bin/luanti --server --world ~/snap/luanti/common/.minetest/worlds/$WORLD --gameid minetest_game --port 30000
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
 sudo systemctl daemon-reload
-sudo systemctl enable sls
-sudo systemctl start sls
+sudo systemctl enable luanti-server
+sudo systemctl start luanti-server
